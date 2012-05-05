a14e8a2a7e3b788ef5cfd5f51a01233cdac630e7
Viacheslav <slgal@slgal-laptop>
2012-05-05 22:31:08 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OtduhNet::Application.config.secret_token = 'ff794bf9a96596c6c1cc4c0d90f85040b1d5cab03eaef8173e39b172a58f765d6675bbcdb301e5b157dbe22a46b6ef63f1d4188aadb1bfe81079742f5146054d'
